19069645c31d5d50300c55cad3959644cbfccf09
Thiago Alves <80608138+Thiagof2755@users.noreply.github.com>
2024-03-13 13:37:56 -0300

    Update Contact

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
 import Style from './Contact.module.scss'
+import { SiInstagram } from "react-icons/si";
+import { SiWhatsapp } from "react-icons/si";
 import { Link } from 'react-router-dom';
+
 
 const Contact = () => {
     return (
         <div className={Style.Container}>
             <h2>Contato</h2>
-            <h5>_________________________________________________________</h5>
+            <h5 className={Style.h5}>_________________________________________________________</h5>
             <div className={Style.Box}>
                 <div className={Style.Local}>
-                    <h4 className={Style.h4}>Brooklin, São Paulo - SP</h4>
-                    <p>Fale conosco direto pelo</p>
-                    <p>Whatsapp: (11) 99655-0605</p>
+                    <h4><Link className={Style.h4} to='https://api.whatsapp.com/message/ZHZ2G2PR7DTVF1?autoload=1&app_absent=0'>
+                        <SiWhatsapp />
+                        <h4>Whatsapp</h4>
+                    </Link>
+                    </h4>
+                    <p>Fale conosco direto pelo <br />
+                    Whatsapp: (34) 991588532</p>
                 </div>
                 <div className={Style.Social}>
-                    <h4 ><Link className={Style.h4} to="https://www.instagram.com/victortattoox/">Instagram</Link></h4>
+                    <h4><Link className={Style.h4} to='https://www.instagram.com/victortattoox/'>
+                        <SiInstagram />
+                        <h4>Instagram</h4>
+                    </Link>
+                    </h4>
                     <p>Confira também, trabalhos, depoimentos e dicas no Instagram</p>
                 </div>
             </div>
